9556ec2ace6b5fc2434f6146d0a598077ca6f2a9
Kevin Guo <kev.guo123@gmail.com>
2024-05-21 21:01:37 -0400

    handle concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,43 @@
 // Uncomment this block to pass the first stage
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
+use std::io;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
+async fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
+    // Primer on sockets: https://docs.python.org/3/howto/sockets.html
+    let mut buf = [0; 512];
+    loop {
+        match stream.read(&mut buf).await {
+            Ok(0) => {
+                // 0 bytes read so connection is closed
+                return Ok(());
+            }
+            Ok(_n) => {
+                // respond with PONG for this part
+                stream.write_all("+PONG\r\n".as_bytes()).await.unwrap();
+            }
+            Err(e) => {
+                return Err(e);
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the fist stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                // Primer on sockets: https://docs.python.org/3/howto/sockets.html
-                let mut buf = [0; 512];
-                loop {
-                    match _stream.read(&mut buf) {
-                        Ok(0) => {
-                            // 0 bytes read so connection is closed
-                            break;
-                        }
-                        Ok(n) => {
-                            // respond with PONG for this part
-                            _stream.write_all("+PONG\r\n".as_bytes()).unwrap();
-                        }
-                        Err(e) => {
-                            eprintln!("Error reading stream: {}", e);
-                            break;
-                        }
-                    }
-                }
+    loop {
+        let (stream, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(stream).await {
+                eprintln!("Error handling connection: {}", e);
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
